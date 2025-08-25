07bb7b3bd9253983a39b201c5c7eb01f23f23601
Poet <946261953@qq.com>
2025-08-25 21:31:49 +0800

    feat: add lession

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 // import './src/drawingLine'
-import './src/index'
+// import './src/index'
 // import './src/begingger'
+import './src/lesson_7'
---
+++ src/lesson_7.js
@@ -0,0 +1,41 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 35, window.innerWidth / window.innerHeight, 0.1, 1000 );
+// const aspectRatio = window.innerWidth / window.innerHeight;
+// const camera = new THREE.OrthographicCamera(
+//     -1 * aspectRatio,
+//     1 * aspectRatio,
+//     1,
+//     -1,
+//     0.1,
+//     200
+// );
+
+const canvas = document.querySelector("canvas.threejs");
+
+const renderer = new THREE.WebGLRenderer({
+    canvas,
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+// document.body.appendChild( renderer.domElement );
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.autoRotate = true;
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "red" });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+// camera.position.x = 2
+
+//
+function animate() {
+    controls.update();
+    renderer.render(scene, camera);
+}
+// animate()
+renderer.setAnimationLoop(animate);
+// renderer.render( scene, camera );
